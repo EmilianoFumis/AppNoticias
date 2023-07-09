c2fffd89179ee5d93c6d254bdf84c367cacc2a9a
Emiliano <emilianofumis>
2023-07-09 15:12:51 -0300

    Initial Commit

---
+++ NoticiasTec/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoticiasTec
+//
+//  Created by Emiliano Fumis on 09/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoticiasTec/NoticiasTecApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoticiasTecApp.swift
+//  NoticiasTec
+//
+//  Created by Emiliano Fumis on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoticiasTecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
